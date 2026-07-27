06c52586c2fbef8acb5c146f110cff2919650dcc
wee <sargsangenrik315@gmail.com>
2026-07-27 13:32:14 +0300

    Refactor RootViewController.h to include device info and monitoring

--- src/RootViewController.h
+++ src/RootViewController.h
@@ -1,4 +1,80 @@
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface RootViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IFDeviceInfo : NSObject
+
+@property (nonatomic, copy, readonly) NSString *identifier;
+@property (nonatomic, copy, readonly) NSString *modelName;
+@property (nonatomic, copy, readonly) NSString *chipName;
+@property (nonatomic, copy, readonly) NSString *architectureName;
+@property (nonatomic, copy, readonly) NSString *imageName;
+@property (nonatomic, copy, readonly) NSString *systemVersion;
+
++ (instancetype)currentDevice;
+
 @end
+
+@interface IFProcessSample : NSObject
+
+@property (nonatomic, assign) pid_t pid;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, assign) uint64_t residentBytes;
+@property (nonatomic, assign) double cpuPercent;
+
+@end
+
+@interface IFProcessMonitor : NSObject
+
+- (NSArray<IFProcessSample *> *)topProcessesByMemory:(NSUInteger)limit;
+- (NSArray<IFProcessSample *> *)topProcessesByCPU:(NSUInteger)limit;
+- (void)refresh;
+
+@end
+
+@interface IFNetworkSnapshot : NSObject
+
+@property (nonatomic, assign) double downloadBytesPerSecond;
+@property (nonatomic, assign) double uploadBytesPerSecond;
+@property (nonatomic, copy) NSString *localIPAddress;
+@property (nonatomic, copy) NSString *activeInterface;
+@property (nonatomic, copy) NSString *vpnInterface;
+@property (nonatomic, copy) NSArray<NSString *> *dnsServers;
+
+@end
+
+@interface IFNetworkMonitor : NSObject
+
+- (IFNetworkSnapshot *)refresh;
++ (void)fetchPublicIPAddressWithCompletion:(void (^)(NSString *address))completion;
+
+@end
+
+@interface IFJailbreakInfo : NSObject
+
+@property (nonatomic, copy) NSString *environmentName;
+@property (nonatomic, copy) NSString *rootPrefix;
+@property (nonatomic, copy) NSString *injectorDescription;
+@property (nonatomic, assign) NSInteger installedPackageCount;
+@property (nonatomic, assign) NSInteger activeTweakCount;
+@property (nonatomic, assign) NSInteger recentCrashCount;
+
+@end
+
+@interface IFetchCore : NSObject
+
++ (uint64_t)totalMemoryBytes;
++ (nullable NSNumber *)usedMemoryBytes;
++ (nullable NSNumber *)totalStorageBytes;
++ (nullable NSNumber *)usedStorageBytes;
++ (NSString *)systemUptime;
++ (NSString *)darwinVersion;
++ (nullable NSString *)batteryCycleCount;
++ (IFJailbreakInfo *)jailbreakInfo;
++ (nullable NSString *)executablePathForCandidates:(NSArray<NSString *> *)candidates;
++ (NSString *)formatBytes:(uint64_t)bytes;
++ (NSString *)formatRate:(double)bytesPerSecond;
+
+@end
+
+NS_ASSUME_NONNULL_END
