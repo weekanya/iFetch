25481ffe29bcf4029358dc59b1eda1a1bb771567
wee <sargsangenrik315@gmail.com>
2026-07-30 13:50:54 +0300

    Prepare iFetch 5.1.1

--- src/core/IFVersion.h
+++ src/core/IFVersion.h
@@ -1,7 +1,7 @@
 #ifndef IFVersion_h
 #define IFVersion_h
 
-#define IFETCH_VERSION @"5.1.0"
-#define IFETCH_BUILD_NUMBER @"510"
+#define IFETCH_VERSION @"5.1.1"
+#define IFETCH_BUILD_NUMBER @"511"
 
 #endif
